d42d3e4d2192d609778e66bf3a9a2ac844c075ab
James Defosse <jam.defosse@gmail.com>
2025-02-12 13:10:53 +0100

    add of housing cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,22 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
 import Banner from '../components/Banner.jsx';
+import styles from '../styles/home.module.scss';
+import HousingCard from '../components/HousingCard';
+import housings from '../data/housings.json';
+
+const listItems = housings.map((housing) => (
+  <li key={housing.id}>
+    <HousingCard housing={housing} />
+  </li>
+));
 
 const Home = () => {
   return (
-    <div>
+    <main>
       <Banner />
-    </div>
+      <ul className={styles['housings-grid']}>{listItems}</ul>
+    </main>
   );
 };
 
