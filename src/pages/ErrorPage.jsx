6555cc08366a66d55cabc4da22edacf81cfae355
James Defosse <jam.defosse@gmail.com>
2025-02-17 15:40:21 +0100

    add of error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,5 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+import styles from '../styles/error.module.scss';
+
 const ErrorPage = () => {
-  return <div>Error Page</div>;
+  const navigate = useNavigate();
+  return (
+    <main className={styles['error-page']}>
+      <h1>404</h1>
+      <p>{`Oups! La page que vous demandez n'existe pas.`}</p>
+      <a href="#" onClick={() => navigate('/')}>
+        {`Retourner sur la page d'accueil`}
+      </a>
+    </main>
+  );
 };
 
 export default ErrorPage;
